7d29c6e30071449a05c582a32ddb65d95674fdbd
hurrhnn <20sunrin022@sunrint.hs.kr>
2022-08-13 15:50:44 +0900

    universal7885: removed duplicate ril rc files.
    Change-Id: Iea3058a7e9b3fa8c8f2a98dbfac9bab9d02d98f1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,22 +25,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.vendor.rilchip.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.vendor.rilchip.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.vendor.rilcommon.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.vendor.rilcommon.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsung.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
